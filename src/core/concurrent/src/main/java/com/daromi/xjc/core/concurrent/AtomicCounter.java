5b113d7cd8d0f3a4e6cc4e1cb5583f5eb35311b5
daromi94 <davromero07@gmail.com>
2025-05-04 16:06:52 -0500

    feat: :sparkles: add atomic counter

---
+++ src/core/concurrent/src/main/java/com/daromi/xjc/core/concurrent/AtomicCounter.java
@@ -0,0 +1,18 @@
+package com.daromi.xjc.core.concurrent;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+final class AtomicCounter implements ConcurrentCounter {
+
+  private final AtomicInteger count = new AtomicInteger(0);
+
+  @Override
+  public void increment() {
+    count.incrementAndGet();
+  }
+
+  @Override
+  public int get() {
+    return count.get();
+  }
+}
