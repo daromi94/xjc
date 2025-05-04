b3fab95a05ed0f546e212bb6997cbbb87d3d2366
daromi94 <davromero07@gmail.com>
2025-05-04 15:59:25 -0500

    feat: :sparkles: add concurrent counter interface

---
+++ src/core/concurrent/src/main/java/com/daromi/xjc/core/concurrent/ConcurrentCounter.java
@@ -0,0 +1,8 @@
+package com.daromi.xjc.core.concurrent;
+
+public interface ConcurrentCounter {
+
+  void increment();
+
+  int get();
+}
--- src/core/concurrent/src/main/java/com/daromi/xjc/core/concurrent/SynchronizedCounter.java
+++ src/core/concurrent/src/main/java/com/daromi/xjc/core/concurrent/SynchronizedCounter.java
@@ -1,17 +1,19 @@
 package com.daromi.xjc.core.concurrent;
 
-public final class SynchronizedCounter {
+final class SynchronizedCounter implements ConcurrentCounter {
 
   private int count = 0;
 
   private final Object lock = new Object();
 
+  @Override
   public void increment() {
     synchronized (lock) {
       count++;
     }
   }
 
+  @Override
   public int get() {
     synchronized (lock) {
       return count;
